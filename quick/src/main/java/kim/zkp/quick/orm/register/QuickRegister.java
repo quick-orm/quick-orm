25e0415f10dc5babe35d8935450d3afe7f2dfa1b
wuije <1250014828@qq.com>
2017-09-19 17:28:02 +0800

    修改注释

--- quick/src/main/java/kim/zkp/quick/orm/register/QuickRegister.java
+++ quick/src/main/java/kim/zkp/quick/orm/register/QuickRegister.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright (c) 2017, ZhuKaipeng 朱开鹏 (2076528290@qq.com).
+
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package kim.zkp.quick.orm.register;
 
 import kim.zkp.quick.orm.sql.convert.FieldConvert;
@@ -8,22 +27,9 @@ import kim.zkp.quick.orm.sql.convert.FieldConvertProcessor;
  * @author     :  zhukaipeng
  * @version    :  1.0  
  * description :  通用组件注册类
- * @see        :                        
  */
 public class QuickRegister {
 	
-	/**
-	 * method name   : registerSqlBuilder 
-	 * description   : 注册sql生成器
-	 * @return       : void
-	 * @param        : @param sBType
-	 * @param        : @param sqlBuilder
-	 * modified      : zhukaipeng ,  2017年8月21日 
-	 * @see          : 
-	 */
-//	public static void registerSqlBuilder(SqlBuilder.SBType sBType,SqlBuilder sqlBuilder) {
-//		SqlBuilderProcessor.registerSqlBuilder(sBType, sqlBuilder);
-//	}
 	/**
 	 * method name   : registerConvert 
 	 * description   : 注册类型转换器（java to db, db to java）
@@ -31,7 +37,6 @@ public class QuickRegister {
 	 * @param        : @param clzz
 	 * @param        : @param ac
 	 * modified      : zhukaipeng ,  2017年8月21日 
-	 * @see          : 
 	 */
 	public static void registerConvert(Class<?> clzz,FieldConvert ac){
 		FieldConvertProcessor.registerConvert(clzz, ac);
